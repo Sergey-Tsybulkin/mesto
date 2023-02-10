e60a2b67415094f4699c1f3e5bfc80760ad8f3a3
Sergey Tsybulkin <tsibulkin.sergey@gmail.com>
2023-02-10 15:47:49 +0300

    feat: add BEM

--- scripts/script.js
+++ scripts/script.js
@@ -9,26 +9,27 @@ let profileSubtitle = document.querySelector(".profile__subtitle");
 
 edit.addEventListener("click", function () {
   popup.classList.add("popup_opened");
-	InputName.value = profileTitle.textContent; 
+  InputName.value = profileTitle.textContent;
   InputAbout.value = profileSubtitle.textContent;
 });
 
 close.addEventListener("click", closingWindow);
 
 popupSave.addEventListener("click", function () {
-  profileTitle.textContent = InputName.value;
-  profileSubtitle.textContent = InputAbout.value;
+  if (InputName.value !== "" && InputAbout.value !== "") {
+    profileTitle.textContent = InputName.value;
+    profileSubtitle.textContent = InputAbout.value;
+  }
   closingWindow();
 });
 
-
-function handleFormSubmit (evt) {
-	evt.preventDefault(); 
-	profileTitle.textContent = InputName.value;
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = InputName.value;
   profileSubtitle.textContent = InputAbout.value;
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+popup.addEventListener("submit", handleFormSubmit);
 
 function closingWindow() {
   popup.classList.remove("popup_opened");
